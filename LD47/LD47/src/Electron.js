3e9e6c2dbedcc3456eb7cdd60f1138ff5179912d
ADA\Josh <jhaxell@gmail.com>
2020-10-03 14:18:33 +0100

    Improved corner centre detection to allow faster electron speeds.

--- LD47/LD47/src/Electron.js
+++ LD47/LD47/src/Electron.js
@@ -22,6 +22,7 @@ export default class Electron
         this.speed = speed;
 
         this.contact = null;
+        this.approach = null;
 
         this.z = 5;
 
@@ -46,16 +47,19 @@ export default class Electron
     {
         var centreMatch = false;
 
-        var xDist = Math.abs(electron.phys.position[0] - contact.phys.position[0]);
-        var yDist = Math.abs(electron.phys.position[1] - contact.phys.position[1]);
+        var axis = 0;
+
+        if(this.approach.axis === "X") axis = 0;
+        else if(this.approach.axis === "Y") axis = 1;
+
+        var dist =  contact.phys.position[axis] - electron.phys.position[axis];
 
         if(this.logging)
         {
             console.log("Centre Dist: x=" + xDist + ", y=" + yDist + ", PIXEL_SCALE=" + PIXEL_SCALE);
         }
 
-        if(xDist <= 1/PIXEL_SCALE
-            && yDist <= 1/PIXEL_SCALE)
+        if(Math.sign(dist) !== this.approach.dir)
         {
             centreMatch = true;
         }
@@ -63,9 +67,40 @@ export default class Electron
         return centreMatch;
     }
 
+    GetXDiff(electron, contact)
+    {
+        return contact.phys.position[0] - electron.phys.position[0];
+    }
+
+    GetYDiff(electron, contact)
+    {
+        return contact.phys.position[1] - electron.phys.position[1];
+    }
+
     SetContact(contact)
     {
         this.contact = contact;
+        
+        var xDiff = this.GetXDiff(this, contact);
+        var yDiff = this.GetYDiff(this, contact);
+
+        if(Math.abs(xDiff) > Math.abs(yDiff))
+        {
+            this.approach = { axis: "X",  dir: Math.sign(xDiff) }
+        }
+        else if(Math.abs(yDiff) > Math.abs(xDiff)) 
+        {
+            this.approach = { axis: "Y", dir: Math.sign(yDiff) }
+        }
+        else
+        {
+            consoleLog("Something has gone horribly wrong in Electron.SetContact");
+            consoleLog(xDiff);
+            consoleLog(yDiff);
+        }
+        
+
+
         if(this.logging)
         {
             consoleLog("CONTACT SET!");
@@ -79,6 +114,14 @@ export default class Electron
         this.velocity = vel;
     }
 
+    SnapToContact(contact)
+    {
+        for(var i = 0; i < contact.phys.position.length; i ++)
+        {
+            this.phys.position[i] = contact.phys.position[i];
+        }
+    }
+
     Update(deltaTime)
     {
         this.phys.velocity = [ this.velocity.x, this.velocity.y ];
@@ -88,6 +131,8 @@ export default class Electron
             if(this.CompareCentre(this, this.contact))
             {
                 var newDir = this.contact.GetFlippedDirection(this.phys.velocity);
+
+                this.SnapToContact(this.contact);
 
                 this.SetVelocity({x: newDir[0] * this.speed, y: newDir[1] * this.speed});
                 this.contact = null;

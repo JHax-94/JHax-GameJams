35fc4d2624cfdffdcbefc4b4519963f04e7589d7
ADA\Josh <jhaxell@gmail.com>
2020-10-05 21:09:15 +0100

    Unblock music

--- LD47/LD47/src/SoundSettings.js
+++ LD47/LD47/src/SoundSettings.js
@@ -26,14 +26,14 @@ export default class SoundSettings
     }
 
     PlaySong(songName)
-    {/*
+    {
         if((this.currentSong != songName && this.songElapsed >= 10) || this.isSongPlaying === false)
         {
             this.songElapsed = 0;
             if(this.soundOn) patatracker.playSong(songName);
             this.currentSong = songName;
             this.isSongPlaying = true;
-        }*/
+        }
     }
 
     PlayRandomSong()

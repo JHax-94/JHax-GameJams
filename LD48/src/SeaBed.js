16cd14d55f88e80be1044206f99629983d5b758c
ADA\Josh <jhaxell@gmail.com>
2021-04-25 10:45:37 +0100

    Added some basic stuff for configuring chart data

--- LD48/src/SeaBed.js
+++ LD48/src/SeaBed.js
@@ -7,6 +7,7 @@ export default class SeaBed
 {
     constructor(mapName)
     {
+        this.mapName = mapName;
         this.map = getMap(mapName);
         tilesheet(assets.tilesheet_dive);
 
@@ -14,37 +15,74 @@ export default class SeaBed
 
         //this.seaBedPos = { x: 0 , y: 30 };
         
+        this.chestMapData = [];
+        this.clamMapData = [];
+        this.bedMapData = [];
+
         this.chests = [];
         this.clams = [];
-
         this.bedTiles = [];
 
         em.AddRender(this);
 
+        this.LoadMapData();
+
+        this.LogMapData();
+        this.ProcessMapData();
+
+    }
+
+    LogMapData()
+    {
+        var chestLog = [];
+        var clamLog = [];
+
+        for(var i = 0; i < this.chestMapData.length; i ++)
+        {
+            var chestTile = this.chestMapData[i];
+
+            consoleLog(chestTile);
+
+            var chestInfo = {
+                type: "CHEST",
+                contents: "",
+                mapLoc: { x: chestTile.x, y: chestTile.y }
+            };
+
+            chestLog.push(chestInfo);
+        }
+
+        for(var i = 0; i < this.clamMapData.length; i ++)
+        {
+            var clamTile = this.clamMapData[i];
+
+            consoleLog(clamTile);
+
+            var clamInfo = {
+                type: "CLAM",
+                pearlId: -1,
+                mapLoc: { x: clamTile.x, y: clamTile.y }
+            };
+
+            clamLog.push(clamInfo);
+        }
+
+        consoleLog("===== CHART DATA FOR MAP: " + this.mapName + " =====");
+        consoleLog("==== CHESTS ====");
+        consoleLog(JSON.stringify(chestLog));
+        consoleLog("==== CLAMS ====");
+        consoleLog(JSON.stringify(clamLog));
+    }
+
+    LoadMapData()
+    {
         for(var i = 0; i < SEABED_COLLISION_TILES.length; i ++)
         {
             var mappedTiles = this.map.find(SEABED_COLLISION_TILES[i]);
 
             for(var j = 0; j < mappedTiles.length; j ++)
             {
-                var tile = mappedTiles[j];
-                /*
-                consoleLog("ADD PHYS FOR TILE: ");
-                consoleLog(tile);*/
-
-                var phys = {
-                    tileTransform: { 
-                        x: this.mapPosition.x + tile.x, 
-                        y: this.mapPosition.y + tile.y,
-                        w: 1,
-                        h: 1
-                    },
-                    isKinematic: true,
-                    isSensor: false,
-                    tag: "SEABED",
-                };
-    
-                this.bedTiles.push(new BedTile(phys));
+                this.bedMapData.push(mappedTiles[j]);
             }
         }
 
@@ -52,11 +90,9 @@ export default class SeaBed
         {
             var mappedTiles = this.map.find(CHEST_TILES[i]);
 
-            for(var j = 0; j < mappedTiles.length; j ++)
+            for(var j = 0; j < mappedTiles.length; j++)
             {
-                var tile = mappedTiles[j];
-
-                this.chests.push(new TreasureChest({x: this.mapPosition.x + tile.x, y: this.mapPosition.y + tile.y }, CHEST_TILES[i], { type: "OXYGEN" }));
+                this.chestMapData.push(mappedTiles[j]);
             }
         }
 
@@ -66,10 +102,44 @@ export default class SeaBed
             
             for(var j = 0; j < mappedTiles.length; j++)
             {
-                var tile = mappedTiles[j];
-
-                this.clams.push(new Clam({ x: tile.x + this.mapPosition.x, y: tile.y + this.mapPosition.y }, CLAM_TILES[i]));
+                this.clamMapData.push(mappedTiles[j]);
             }
+        }
+    }
+
+    ProcessMapData()
+    {
+        for(var i = 0; i < this.bedMapData.length; i ++)
+        {
+            var tile = this.bedMapData[i];
+
+            var phys = {
+                tileTransform: { 
+                    x: this.mapPosition.x + tile.x, 
+                    y: this.mapPosition.y + tile.y,
+                    w: 1,
+                    h: 1
+                },
+                isKinematic: true,
+                isSensor: false,
+                tag: "SEABED",
+            };
+
+            this.bedTiles.push(new BedTile(phys));
+        }
+
+        for(var i = 0; i < this.chestMapData.length; i ++)
+        {
+            var tile = this.chestMapData[i];
+
+            this.chests.push(new TreasureChest({x: this.mapPosition.x + tile.x, y: this.mapPosition.y + tile.y }, tile.index, { type: "OXYGEN" }));
+        }
+
+        for(var i = 0; i < this.clamMapData.length; i++)
+        {
+            var tile = this.clamMapData[i];
+
+            this.clams.push(new Clam({ x: tile.x + this.mapPosition.x, y: tile.y + this.mapPosition.y }, tile.index));
         }
     }
 

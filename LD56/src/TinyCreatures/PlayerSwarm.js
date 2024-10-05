7ae7d9aa81c5f80f81ae458078af975172223539
JHax-94 <jhaxell@gmail.com>
2024-10-05 10:32:56 +0100

    Add some movement in
    Had a bit of a play around with camera smoothing

--- LD56/src/TinyCreatures/PlayerSwarm.js
+++ LD56/src/TinyCreatures/PlayerSwarm.js
@@ -1,9 +1,12 @@
-import { COLLISION_GROUP, EM, PIXEL_SCALE, TILE_HEIGHT, TILE_WIDTH } from "../main"
+import { vec2 } from "p2";
+import { COLLISION_GROUP, consoleLog, EM, PIXEL_SCALE, TILE_HEIGHT, TILE_WIDTH } from "../main"
 
 export default class PlayerSwarm
 {
     constructor(pos)
     {
+        this.speed = 2*PIXEL_SCALE;
+
         let physSettings = {
             tileTransform: { x: pos.x, y: pos.y, w: 1, h: 1 },
             mass: 100,
@@ -20,6 +23,42 @@ export default class PlayerSwarm
         EM.RegisterEntity(this, { physSettings: physSettings });
     }
 
+    Input(input)
+    {
+        let inputVector = { x: 0, y: 0 };
+
+        if(input.right)
+        {
+            inputVector.x = 1;
+        }
+        else if(input.left)
+        {
+            inputVector.x = -1;
+        }
+        else
+        {
+            inputVector.x = 0;
+        }
+
+        if(input.up)
+        {
+            inputVector.y = 1;            
+        }
+        else if(input.down)
+        {
+            inputVector.y = -1;
+        }
+        else
+        {
+            inputVector.y = 0;
+        }
+
+        let moveVec = [0,0];
+        vec2.normalize(moveVec, [inputVector.x, inputVector.y]);
+
+        this.phys.velocity = [moveVec[0] * this.speed, moveVec[1] * this.speed];
+    }
+
     Update(deltaTime)
     {
         let physPos = this.phys.position;
@@ -27,14 +66,14 @@ export default class PlayerSwarm
         let hWidth = 0.5 * TILE_WIDTH * PIXEL_SCALE;
         let hHeight = 0.5 * TILE_HEIGHT * PIXEL_SCALE;
 
-        EM.camera.MoveTo(physPos[0]-hWidth, physPos[1]+hHeight);
+        EM.camera.MoveTo(Math.floor(physPos[0]-hWidth), Math.floor(physPos[1]+hHeight));
     }
 
     Draw()
     {
         let screenPos = this.GetScreenPos();
 
-        EM.hudLog.push(`PlayerPos: (${screenPos.x}, ${screenPos.y})`);
+        
 
         paper(6);
         rectf(screenPos.x, screenPos.y, PIXEL_SCALE, PIXEL_SCALE);
